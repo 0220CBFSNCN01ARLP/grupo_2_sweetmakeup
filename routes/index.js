b1fb96a81811775f04a6e906177d19bf98da5a63
genaromalpeli <genamalpeli@gmail.com>
2020-05-13 01:52:16 -0300

    ordeno codigo y comento

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("home");
+  res.render("index");
 });
 
 router.get("/register", function (req, res, next) {
