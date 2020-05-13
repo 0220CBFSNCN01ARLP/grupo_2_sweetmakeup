4c4e922b01214926388cee838d7511303a78e8e0
agustinherlein <agustinherlein@hotmail.com>
2020-05-13 02:16:49 -0300

    cambios a la página de sección con ejs, eliminación de carpeta node_modules

--- routes/index.js
+++ routes/index.js
@@ -14,8 +14,20 @@ router.get("/cart", function (req, res, next) {
   res.render("productCart");
 });
 
-router.get("/section/:id", function (req, res, next) {
-  res.render("section-" + req.params.id);
+router.get("/ojos", function (req, res, next) {
+  res.render("section", { title: "ojos", subsections: ["sombra", "delineador", "corrector de ojeras", "mascara"] });
+});
+
+router.get("/labios", function (req, res, next) {
+  res.render("section", { title: "labios", subsections: ["lápiz labial", "delineador", "lorem", "ipsum"] });
+});
+
+router.get("/cara", function (req, res, next) {
+  res.render("section", { title: "cara", subsections: ["base", "rubor", "lorem", "mascara"] });
+});
+
+router.get("/cejas", function (req, res, next) {
+  res.render("section", { title: "cejas", subsections: ["lorem", "ipsum", "corrector de ojeras", "mascara"] });
 });
 
 module.exports = router;
