36fab869ea8cc502901e5d52f5be522169801ab1
genaromalpeli <genamalpeli@gmail.com>
2020-05-13 14:10:35 -0300

    modifico nombre producto a productDetail

--- routes/products.js
+++ routes/products.js
@@ -1,8 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
-router.get('/:id?', function(req, res, next) {
-    res.render('producto')
-  });
+router.get("/:id?", function (req, res, next) {
+  res.render("productDetail");
+});
 
 module.exports = router;
