699466a83c8ab83a3364cc084f754b91543b2b1c
genaromalpeli <genamalpeli@gmail.com>
2020-05-13 00:59:32 -0300

    finalizo home y carga de producto

--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-router.get('/detail/:id?', function(req, res, next) {
-    res.render('producto')
-  });
-  
-router.get('/load', function(req, res, next) {
-    res.render('loadProduct')
-  });
+router.get("/detail/:id?", function (req, res, next) {
+  res.render("producto");
+});
+
+router.get("/add", function (req, res, next) {
+  res.render("productAdd");
+});
+
+router.get("/add2", function (req, res, next) {
+  res.render("loadedProducts");
+});
 
 module.exports = router;
