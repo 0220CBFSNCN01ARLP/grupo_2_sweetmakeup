b1da0bae63c9358f9249566af870edde55a16445
Fernando-Toma <tomasellif@hotmail.com>
2020-06-16 21:38:35 -0300

    structure.sql actualizado

--- structure.sql
+++ structure.sql
@@ -1,55 +1,45 @@
--- proyecto.brands definition
+CREATE DATABASE `PROYECTO`
 
-CREATE TABLE `brands` (
+CREATE TABLE proyecto.brands (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(500) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) 
 
 
--- proyecto.categories definition
-
-CREATE TABLE `categories` (
+CREATE TABLE proyecto.categories (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(500) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) 
 
 
--- proyecto.color definition
-
-CREATE TABLE `color` (
+CREATE TABLE proyecto.colors (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(500) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) 
 
 
--- proyecto.tags definition
-
-CREATE TABLE `tags` (
+CREATE TABLE proyecto.tags (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(500) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) 
 
 
--- proyecto.users definition
-
-CREATE TABLE `users` (
+CREATE TABLE proyecto.users (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(500) DEFAULT NULL,
-  `lastName` varchar(500) DEFAULT NULL,
+  `last_name` varchar(500) DEFAULT NULL,
   `mail` varchar(100) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
   `avatar` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) 
 
 
--- proyecto.addresses definition
-
-CREATE TABLE `addresses` (
+CREATE TABLE proyecto.addresses (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `street` varchar(100) DEFAULT NULL,
   `number` varchar(100) DEFAULT NULL,
@@ -61,16 +51,14 @@ CREATE TABLE `addresses` (
   `phone` varchar(100) DEFAULT NULL,
   `neighborhood` varchar(100) DEFAULT NULL,
   `observations` varchar(100) DEFAULT NULL,
-  `user_id` int(11) NOT NULL,
+  `users_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `addresses_fk` (`user_id`),
-  CONSTRAINT `addresses_fk` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  KEY `addresses_fk` (`users_id`),
+  CONSTRAINT `addresses_fk` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
+) 
 
 
--- proyecto.products definition
-
-CREATE TABLE `products` (
+CREATE TABLE proyecto.products (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(500) NOT NULL,
   `description` varchar(500) DEFAULT NULL,
@@ -81,37 +69,33 @@ CREATE TABLE `products` (
   `height` decimal(6,2) DEFAULT NULL,
   `width` decimal(6,2) DEFAULT NULL,
   `length` decimal(6,2) DEFAULT NULL,
-  `brand_id` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL,
+  `brands_id` int(11) NOT NULL,
+  `categories_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `products_fk` (`category_id`),
-  KEY `products_fk_1` (`brand_id`),
-  CONSTRAINT `products_fk` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
-  CONSTRAINT `products_fk_1` FOREIGN KEY (`brand_id`) REFERENCES `brands` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  KEY `products_fk` (`categories_id`),
+  KEY `products_fk_1` (`brands_id`),
+  CONSTRAINT `products_fk` FOREIGN KEY (`categories_id`) REFERENCES `categories` (`id`),
+  CONSTRAINT `products_fk_1` FOREIGN KEY (`brands_id`) REFERENCES `brands` (`id`)
+) 
 
 
--- proyecto.purchases definition
-
-CREATE TABLE `purchases` (
+CREATE TABLE proyecto.purchases (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
+  `users_id` int(11) NOT NULL,
   `date` date DEFAULT NULL,
   `total` decimal(6,2) DEFAULT NULL,
   `payment` varchar(100) DEFAULT NULL,
   `invoice` varchar(100) DEFAULT NULL,
-  `address_id` int(11) NOT NULL,
+  `addresses_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `purchases_fk` (`address_id`),
-  KEY `purchases_fk_1` (`user_id`),
-  CONSTRAINT `purchases_fk` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`id`),
-  CONSTRAINT `purchases_fk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  KEY `purchases_fk` (`addresses_id`),
+  KEY `purchases_fk_1` (`users_id`),
+  CONSTRAINT `purchases_fk` FOREIGN KEY (`addresses_id`) REFERENCES `addresses` (`id`),
+  CONSTRAINT `purchases_fk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
+) 
 
 
--- proyecto.user_product definition
-
-CREATE TABLE `user_product` (
+CREATE TABLE proyecto.user_product (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `users_id` int(11) NOT NULL,
   `products_id` int(11) NOT NULL,
@@ -120,60 +104,56 @@ CREATE TABLE `user_product` (
   KEY `user_product_fk_1` (`users_id`),
   CONSTRAINT `user_product_fk` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`),
   CONSTRAINT `user_product_fk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) 
 
 
--- proyecto.color_product definition
-
-CREATE TABLE `color_product` (
+CREATE TABLE proyecto.color_product (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `color_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
+  `colors_id` int(11) NOT NULL,
+  `products_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `color_product_fk` (`color_id`),
-  KEY `color_product_fk_1` (`product_id`),
-  CONSTRAINT `color_product_fk` FOREIGN KEY (`color_id`) REFERENCES `color` (`id`),
-  CONSTRAINT `color_product_fk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  KEY `color_product_fk` (`colors_id`),
+  KEY `color_product_fk_1` (`products_id`),
+  CONSTRAINT `color_product_fk` FOREIGN KEY (`colors_id`) REFERENCES `colors` (`id`),
+  CONSTRAINT `color_product_fk_1` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`)
+) 
 
 
--- proyecto.images definition
-
-CREATE TABLE `images` (
+CREATE TABLE proyecto.images (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `route` varchar(500) NOT NULL,
   `size` varchar(500) DEFAULT NULL,
   `fileType` varchar(500) DEFAULT NULL,
-  `product_id` int(11) NOT NULL,
+  `products_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `images_fk` (`product_id`),
-  CONSTRAINT `images_fk` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  KEY `images_fk` (`products_id`),
+  CONSTRAINT `images_fk` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`)
+) 
 
 
--- proyecto.product_purchase definition
-
-CREATE TABLE `product_purchase` (
+CREATE TABLE proyecto.product_purchase (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `purchase_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
+  `purchase_ids` int(11) NOT NULL,
+  `product_ids` int(11) NOT NULL,
+  `price` decimal(6,2) DEFAULT NULL,
+  `quantity` decimal(6,2) DEFAULT NULL,
+  `products_id` int(11) NOT NULL,
+  `purchases_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `product_purchase_fk` (`product_id`),
-  KEY `product_purchase_fk_1` (`purchase_id`),
-  CONSTRAINT `product_purchase_fk` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
-  CONSTRAINT `product_purchase_fk_1` FOREIGN KEY (`purchase_id`) REFERENCES `purchases` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  KEY `product_purchase_fk` (`products_id`),
+  KEY `product_purchase_fk_1` (`purchases_id`),
+  CONSTRAINT `product_purchase_fk` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`),
+  CONSTRAINT `product_purchase_fk_1` FOREIGN KEY (`purchases_id`) REFERENCES `purchases` (`id`)
+) 
 
 
--- proyecto.product_tag definition
-
-CREATE TABLE `product_tag` (
+CREATE TABLE proyecto.product_tag (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tag_id` int(11) NOT NULL,
-  `product_id` int(11) NOT NULL,
+  `tags_id` int(11) NOT NULL,
+  `products_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `product_tag_fk` (`tag_id`),
-  KEY `product_tag_fk_1` (`product_id`),
-  CONSTRAINT `product_tag_fk` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`),
-  CONSTRAINT `product_tag_fk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  KEY `product_tag_fk` (`tags_id`),
+  KEY `product_tag_fk_1` (`products_id`),
+  CONSTRAINT `product_tag_fk` FOREIGN KEY (`tags_id`) REFERENCES `tags` (`id`),
+  CONSTRAINT `product_tag_fk_1` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`)
+)
