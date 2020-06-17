78519ee074638476656a534aeee739407336eb06
Fernando-Toma <tomasellif@hotmail.com>
2020-06-17 20:57:14 -0300

    structure.sql

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,83 +4,90 @@ const bcrypt = require("bcrypt");
 const multer = require("multer");
 var express = require("express");
 
-const { getUsers, usersFilePath } = require("../utils/users");
+const {
+    getUsers,
+    usersFilePath
+} = require("../utils/users");
 
-let { check, validationResult, body } = require("express-validator");
+let {
+    check,
+    validationResult,
+    body
+} = require("express-validator");
 
 let usersController = {
-  showRegister: (req, res) => {
-    res.render("register");
-  },
+    showRegister: (req, res) => {
+        res.render("register");
+    },
 
-  register: (req, res, next) => {
-    let errors = validationResult(req);
+    register: (req, res, next) => {
+        let errors = validationResult(req);
 
-    if (errors.isEmpty()) {
-      const users = getUsers();
-      req.body.password = bcrypt.hashSync(req.body.password, 10);
+        if (errors.isEmpty()) {
+            const users = getUsers();
+            req.body.password = bcrypt.hashSync(req.body.password, 10);
 
-      const read = JSON.parse(
-        fs.readFileSync(path.resolve(__dirname, "../data/users.json"))
-      );
+            const read = JSON.parse(
+                fs.readFileSync(path.resolve(__dirname, "../data/users.json"))
+            );
 
-      const maxId = Math.max(...users.map((o) => o.id), 0);
-      let newId = maxId + 1;
+            const maxId = Math.max(...users.map((o) => o.id), 0);
+            let newId = maxId + 1;
 
-      let user = {
-        id: newId,
-        ...req.body,
-        avatar: req.files[0].filename,
-      };
+            let user = {
+                id: newId,
+                ...req.body,
+                avatar: req.files[0].filename,
+            };
 
-      users.push(user);
+            users.push(user);
 
-      fs.writeFileSync(
-        path.resolve(__dirname, "../data/users.json"),
-        JSON.stringify(users)
-      );
+            fs.writeFileSync(
+                path.resolve(__dirname, "../data/users.json"),
+                JSON.stringify(users)
+            );
 
-      res.redirect("/");
-    } else {
-      return res.render("register", {
-        errors: errors.errors,
-      });
-    }
-  },
+            res.redirect("/");
+        } else {
+            return res.render("register", {
+                errors: errors.errors,
+            });
+        }
+    },
 
-  login: (req, res, next) => {
-    const users = getUsers();
-    const user = users.find((e) => {
-      return (
-        bcrypt.compareSync(req.body.loginPassword, e.password) &&
-        e.email == req.body.loginEmail
-      );
-    });
-    if (user == null) return res.redirect("register");
+    login: (req, res, next) => {
+        const users = getUsers();
+        const user = users.find((e) => {
+            return (
+                bcrypt.compareSync(req.body.loginPassword, e.password) &&
+                e.email == req.body.loginEmail
+            );
+        });
+        if (user == null) return res.redirect("register");
 
-    // Se guarda la cookie por 30 minutos, el usuario puede cerrar el navegador y volver al poco tiempo
-    let cookieAge = 1800000;
-    // Si selecciona recordar, la cookie dura un mes
-    if (req.body.remember != undefined) {
-      cookieAge = 2630000000;
-    }
-    req.session.user = user;
-    res.cookie("user", user.email, {
-      maxAge: cookieAge,
-    });
-    res.redirect("/");
-  },
-
-  logout: (req, res, next) => {
-    if (req.session.user != null) {
-      req.session.destroy(function (err) {
-        res.clearCookie("user");
+        // Se guarda la cookie por 30 minutos, el usuario puede cerrar el navegador y volver al poco tiempo
+        let cookieAge = 1800000;
+        // Si selecciona recordar, la cookie dura un mes
+        if (req.body.remember != undefined) {
+            cookieAge = 2630000000;
+        }
+        req.session.user = user;
+        res.cookie("user", user.email, {
+            maxAge: cookieAge,
+        });
         res.redirect("/");
-      });
-    } else {
-      res.redirect("register");
-    }
-  },
+    },
+
+    logout: (req, res, next) => {
+        if (req.session.user != null) {
+            req.session.destroy(function(err) {
+                res.clearCookie("user");
+                res.redirect("/");
+            });
+        } else {
+            res.redirect("register");
+        }
+    },
 };
 
 module.exports = usersController;
--- structure.sql
+++ structure.sql
@@ -1,64 +1,82 @@
-CREATE DATABASE `PROYECTO`
+CREATE DATABASE `grupo_2_sweetmakeup`;
 
-CREATE TABLE proyecto.brands (
+USE `grupo_2_sweetmakeup`;
+
+CREATE TABLE `brands` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(500) DEFAULT NULL,
+  `createdAt` DATETIME,
+   `updatedAt` DATETIME,
   PRIMARY KEY (`id`)
 ) 
 
 
-CREATE TABLE proyecto.categories (
+CREATE TABLE `categories` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(500) NOT NULL,
+  `createdAt` DATETIME,
+   `updatedAt` DATETIME,
   PRIMARY KEY (`id`)
 ) 
 
 
-CREATE TABLE proyecto.colors (
+
+CREATE TABLE `colors` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(500) NOT NULL,
+  `createdAt` DATETIME,
+   `updatedAt` DATETIME,
   PRIMARY KEY (`id`)
 ) 
 
 
-CREATE TABLE proyecto.tags (
+
+CREATE TABLE `tags` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(500) NOT NULL,
+  `createdAt` DATETIME,
+   `updatedAt` DATETIME,
   PRIMARY KEY (`id`)
 ) 
 
 
-CREATE TABLE proyecto.users (
+
+CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(500) DEFAULT NULL,
-  `last_name` varchar(500) DEFAULT NULL,
-  `mail` varchar(100) DEFAULT NULL,
+  `firstName` varchar(500) DEFAULT NULL,
+  `lastName` varchar(500) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
   `avatar` varchar(100) DEFAULT NULL,
+  `createdAt` DATETIME,
+   `updatedAt` DATETIME,
   PRIMARY KEY (`id`)
 ) 
 
 
-CREATE TABLE proyecto.addresses (
+CREATE TABLE `addresses` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `street` varchar(100) DEFAULT NULL,
   `number` varchar(100) DEFAULT NULL,
   `city` varchar(100) DEFAULT NULL,
   `province` varchar(100) DEFAULT NULL,
-  `zipcode` varchar(100) DEFAULT NULL,
+  `zipCode` varchar(100) DEFAULT NULL,
   `floor` varchar(100) DEFAULT NULL,
   `apartment` varchar(100) DEFAULT NULL,
   `phone` varchar(100) DEFAULT NULL,
   `neighborhood` varchar(100) DEFAULT NULL,
   `observations` varchar(100) DEFAULT NULL,
-  `users_id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `createdAt` DATETIME,
+   `updatedAt` DATETIME,
   PRIMARY KEY (`id`),
-  KEY `addresses_fk` (`users_id`),
-  CONSTRAINT `addresses_fk` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
+  KEY `addresses_fk` (`userId`),
+  CONSTRAINT `addresses_fk` FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
 ) 
 
 
-CREATE TABLE proyecto.products (
+
+CREATE TABLE `products` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(500) NOT NULL,
   `description` varchar(500) DEFAULT NULL,
@@ -69,91 +87,101 @@ CREATE TABLE proyecto.products (
   `height` decimal(6,2) DEFAULT NULL,
   `width` decimal(6,2) DEFAULT NULL,
   `length` decimal(6,2) DEFAULT NULL,
-  `brands_id` int(11) NOT NULL,
-  `categories_id` int(11) NOT NULL,
+  `brandId` int(11) NOT NULL,
+  `categoryId` int(11) NOT NULL,
+  `createdAt` DATETIME,
+   `updatedAt` DATETIME,
   PRIMARY KEY (`id`),
-  KEY `products_fk` (`categories_id`),
-  KEY `products_fk_1` (`brands_id`),
-  CONSTRAINT `products_fk` FOREIGN KEY (`categories_id`) REFERENCES `categories` (`id`),
-  CONSTRAINT `products_fk_1` FOREIGN KEY (`brands_id`) REFERENCES `brands` (`id`)
+  KEY `products_fk` (`categoryId`),
+  KEY `products_fk_1` (`brandId`),
+  CONSTRAINT `products_fk` FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`),
+  CONSTRAINT `products_fk_1` FOREIGN KEY (`brandId`) REFERENCES `brands` (`id`)
 ) 
 
 
-CREATE TABLE proyecto.purchases (
+
+CREATE TABLE `purchases` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `users_id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
   `date` date DEFAULT NULL,
   `total` decimal(6,2) DEFAULT NULL,
   `payment` varchar(100) DEFAULT NULL,
   `invoice` varchar(100) DEFAULT NULL,
-  `addresses_id` int(11) NOT NULL,
+  `addressId` int(11) NOT NULL,
+  `createdAt` DATETIME,
+   `updatedAt` DATETIME,
   PRIMARY KEY (`id`),
-  KEY `purchases_fk` (`addresses_id`),
-  KEY `purchases_fk_1` (`users_id`),
-  CONSTRAINT `purchases_fk` FOREIGN KEY (`addresses_id`) REFERENCES `addresses` (`id`),
-  CONSTRAINT `purchases_fk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
+  KEY `purchases_fk` (`addressId`),
+  KEY `purchases_fk_1` (`userId`),
+  CONSTRAINT `purchases_fk` FOREIGN KEY (`addressId`) REFERENCES `addresses` (`id`),
+  CONSTRAINT `purchases_fk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
 ) 
 
 
-CREATE TABLE proyecto.user_product (
+
+
+CREATE TABLE `user_product` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `users_id` int(11) NOT NULL,
-  `products_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_product_fk` (`products_id`),
-  KEY `user_product_fk_1` (`users_id`),
-  CONSTRAINT `user_product_fk` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`),
-  CONSTRAINT `user_product_fk_1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`)
+  `userId` int(11) NOT NULL,
+  `productId` int(11) NOT NULL,
+   PRIMARY KEY (`id`),
+  KEY `user_product_fk` (`productId`),
+  KEY `user_product_fk_1` (`userId`),
+  CONSTRAINT `user_product_fk` FOREIGN KEY (`productId`) REFERENCES `products` (`id`),
+  CONSTRAINT `user_product_fk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`)
 ) 
 
 
-CREATE TABLE proyecto.color_product (
+
+CREATE TABLE `color_product` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `colors_id` int(11) NOT NULL,
-  `products_id` int(11) NOT NULL,
+  `colorId` int(11) NOT NULL,
+  `productId` int(11) NOT NULL,2
   PRIMARY KEY (`id`),
-  KEY `color_product_fk` (`colors_id`),
-  KEY `color_product_fk_1` (`products_id`),
-  CONSTRAINT `color_product_fk` FOREIGN KEY (`colors_id`) REFERENCES `colors` (`id`),
-  CONSTRAINT `color_product_fk_1` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`)
+  KEY `color_product_fk` (`colorId`),
+  KEY `color_product_fk_1` (`productId`),
+  CONSTRAINT `color_product_fk` FOREIGN KEY (`colorId`) REFERENCES `colors` (`id`),
+  CONSTRAINT `color_product_fk_1` FOREIGN KEY (`productId`) REFERENCES `products` (`id`)
 ) 
 
 
-CREATE TABLE proyecto.images (
+
+CREATE TABLE `images` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `route` varchar(500) NOT NULL,
   `size` varchar(500) DEFAULT NULL,
   `fileType` varchar(500) DEFAULT NULL,
-  `products_id` int(11) NOT NULL,
+  `productId` int(11) NOT NULL,
+  `createdAt` DATETIME,
+   `updatedAt` DATETIME,
   PRIMARY KEY (`id`),
-  KEY `images_fk` (`products_id`),
-  CONSTRAINT `images_fk` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`)
+  KEY `images_fk` (`productId`),
+  CONSTRAINT `images_fk` FOREIGN KEY (`productId`) REFERENCES `products` (`id`)
 ) 
 
 
-CREATE TABLE proyecto.product_purchase (
+
+CREATE TABLE `product_purchase` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `purchase_ids` int(11) NOT NULL,
-  `product_ids` int(11) NOT NULL,
   `price` decimal(6,2) DEFAULT NULL,
   `quantity` decimal(6,2) DEFAULT NULL,
-  `products_id` int(11) NOT NULL,
-  `purchases_id` int(11) NOT NULL,
+  `productId` int(11) NOT NULL,
+  `purchaseId` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `product_purchase_fk` (`products_id`),
-  KEY `product_purchase_fk_1` (`purchases_id`),
-  CONSTRAINT `product_purchase_fk` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`),
-  CONSTRAINT `product_purchase_fk_1` FOREIGN KEY (`purchases_id`) REFERENCES `purchases` (`id`)
+  KEY `product_purchase_fk` (`productId`),
+  KEY `product_purchase_fk_1` (`purchaseId`),
+  CONSTRAINT `product_purchase_fk` FOREIGN KEY (`productId`) REFERENCES `products` (`id`),
+  CONSTRAINT `product_purchase_fk_1` FOREIGN KEY (`purchaseId`) REFERENCES `purchases` (`id`)
 ) 
 
 
-CREATE TABLE proyecto.product_tag (
+CREATE TABLE `product_tag` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `tags_id` int(11) NOT NULL,
-  `products_id` int(11) NOT NULL,
+  `tagId` int(11) NOT NULL,
+  `productId` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `product_tag_fk` (`tags_id`),
-  KEY `product_tag_fk_1` (`products_id`),
-  CONSTRAINT `product_tag_fk` FOREIGN KEY (`tags_id`) REFERENCES `tags` (`id`),
-  CONSTRAINT `product_tag_fk_1` FOREIGN KEY (`products_id`) REFERENCES `products` (`id`)
+  KEY `product_tag_fk` (`tagId`),
+  KEY `product_tag_fk_1` (`productId`),
+  CONSTRAINT `product_tag_fk` FOREIGN KEY (`tagId`) REFERENCES `tags` (`id`),
+  CONSTRAINT `product_tag_fk_1` FOREIGN KEY (`productId`) REFERENCES `products` (`id`)
 )
