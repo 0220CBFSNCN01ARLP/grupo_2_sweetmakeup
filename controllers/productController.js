07e232d6315e3f995b5d5a7a362b5536722b6a56
Fernando-Toma <tomasellif@hotmail.com>
2020-06-27 22:10:16 -0300

    create product db

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,120 +5,136 @@ const multer = require("multer");
 // READ: Agus
 // DELETE: Gena
 
-const { getProducts, productsFilePath } = require("../utils/products");
-const { Product } = require("../database/models");
+const {
+    getProducts,
+    productsFilePath
+} = require("../utils/products");
+const {
+    Product,
+    Category,
+    Brand
+} = require("../database/models");
 
 let controller = {
-  // FER
-  create: function (req, res, next) {
-    res.render("productAdd", {
-      user: req.session.user,
-    });
-  },
+    // FER
+    create: async function(req, res, next) {
+        const categories = await Category.findAll();
+        const brands = await Brand.findAll();
+        res.render("productAdd", {
+            categories,
+            brands,
+            user: req.session.user
+        });
+    },
 
-  // FER
+    // FER
 
-  store: function (req, res, next) {
-    const products = getProducts();
-    //buscar el ID máximo entre los productos existentes, y setear el nuevo como maxId + 1 para asegurar que no se repitan
-    const maxId = Math.max(...products.map((o) => o.id), 0);
-    let newId = maxId + 1;
-    let newProduct = {
-      id: newId,
-      ...req.body,
-      avatar: req.files[0].filename,
-    };
-    console.log(newProduct);
-    products.push(newProduct);
-    fs.writeFileSync(productsFilePath, JSON.stringify(products), "utf-8");
-    res.redirect("/");
-  },
+    store: async function(req, res, next) {
+        console.log("fer");
+        console.log(req.body.thematic);
+        const newProduct = await Product.create({
+            name: req.body.productName,
+            price: req.body.price,
+            brandId: req.body.brand,
+            categoryId: req.body.thematic,
+            discount: req.body.discount,
+            description: req.body.description,
+            weight: req.body.peso,
+            height: req.body.alto,
+            width: req.body.ancho,
+            length: req.body.largo
+        });
+        console.log(newProduct);
+        res.redirect(`/products/${newProduct.id}`);
+    },
 
-  // FER
-  edit: (req, res) => {
-    //GET -> muestra el formulario
+    // FER
+    edit: (req, res) => {
+        //GET -> muestra el formulario
 
-    const products = getProducts();
-    const product = products.find((e) => {
-      return e.id == req.params.id;
-    });
+        const products = getProducts();
+        const product = products.find((e) => {
+            return e.id == req.params.id;
+        });
 
-    if (product == null) return res.redirect("/");
+        if (product == null) return res.redirect("/");
 
-    res.render("productEdit", {
-      product,
-      user: req.session.user,
-    });
-  },
+        res.render("productEdit", {
+            product,
+            user: req.session.user,
+        });
+    },
 
-  //FER
+    //FER
 
-  //PUT /products/edit/12385
-  update: (req, res, next) => {
-    //PUT -> procesar el formulario y redireccionar(o renderizar)
-    const products = getProducts();
-    let product = products.find((e) => {
-      return e.id == req.params.id;
-    });
+    //PUT /products/edit/12385
+    update: (req, res, next) => {
+        //PUT -> procesar el formulario y redireccionar(o renderizar)
+        const products = getProducts();
+        let product = products.find((e) => {
+            return e.id == req.params.id;
+        });
 
-    if (product == null) return res.redirect("/");
+        if (product == null) return res.redirect("/");
 
-    //MODIFICAR EL PRODUCTO EN BASE A LO QUE VIENE DEL FORM y VERIFICAR SI LOS DATOS SON VALIDOS
+        //MODIFICAR EL PRODUCTO EN BASE A LO QUE VIENE DEL FORM y VERIFICAR SI LOS DATOS SON VALIDOS
 
-    req.body.price = Number.parseFloat(req.body.price);
-    req.body.discount = Number.parseFloat(req.body.discount);
+        req.body.price = Number.parseFloat(req.body.price);
+        req.body.discount = Number.parseFloat(req.body.discount);
 
-    //ACTUALIZAR LOS DATOS DEL PRODUCTO
-    product = {
-      ...product,
-      ...req.body,
-    };
+        //ACTUALIZAR LOS DATOS DEL PRODUCTO
+        product = {
+            ...product,
+            ...req.body,
+        };
 
-    //GUARDAR EL PRODUCTO EN LA DB
-    const index = products.findIndex((product, index) => {
-      return product.id == req.params.id;
-    });
-    products.splice(index, 1, product);
+        //GUARDAR EL PRODUCTO EN LA DB
+        const index = products.findIndex((product, index) => {
+            return product.id == req.params.id;
+        });
+        products.splice(index, 1, product);
 
-    fs.writeFileSync(productsFilePath, JSON.stringify(products), "utf-8");
+        fs.writeFileSync(productsFilePath, JSON.stringify(products), "utf-8");
 
-    res.redirect("/products/" + product.id);
-  },
+        res.redirect("/products/" + product.id);
+    },
 
-  //GENARO
+    //GENARO
 
-  destroy: (req, res) => {
-    const products = getProducts();
-    const index = products.findIndex((e) => {
-      return e.id == req.params.id;
-    });
+    destroy: (req, res) => {
+        const products = getProducts();
+        const index = products.findIndex((e) => {
+            return e.id == req.params.id;
+        });
 
-    if (index == -1) return res.redirect("/");
+        if (index == -1) return res.redirect("/");
 
-    products.splice(index, 1);
+        products.splice(index, 1);
 
-    fs.writeFileSync(productsFilePath, JSON.stringify(products), "utf-8");
+        fs.writeFileSync(productsFilePath, JSON.stringify(products), "utf-8");
 
-    res.redirect("/");
-  },
+        res.redirect("/");
+    },
 
-  //AGUS
-  detail: async function (req, res, next) {
-    let product = await Product.findByPk(req.params.id);
-    let related = [];
-    for (let i = 0; i < 4; i++) {
-      let relatedProduct = await Product.findOne({
-        where: { categoryId: product.categoryId },
-      });
-      related.push(relatedProduct)
-    };
+    //AGUS
+    detail: async function(req, res, next) {
+        let product = await Product.findByPk(req.params.id);
+        let related = [];
+        for (let i = 0; i < 4; i++) {
+            let relatedProduct = await Product.findOne({
+                where: {
+                    categoryId: product.categoryId
+                },
+            });
+            related.push(relatedProduct)
+        };
 
-    res.render("productDetail", {
-      product,
-      related,
-      user: req.session.user,
-    });
-  },
+        res.render("productDetail", {
+            product,
+            related,
+            user: req.session.user,
+        });
+    },
 };
 
 module.exports = controller;
--- structure.sql
+++ structure.sql
@@ -2,171 +2,287 @@ CREATE DATABASE `grupo_2_sweetmakeup`;
 
 USE `grupo_2_sweetmakeup`;
 
-CREATE TABLE brands (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  name varchar(500) DEFAULT NULL,
+CREATE TABLE brands
+(
+  id int(11) NOT NULL
+  AUTO_INCREMENT,
+  name varchar
+  (500) DEFAULT NULL,
   createdAt DATETIME,
   updatedAt DATETIME,
-  PRIMARY KEY (id)
-); 
+  PRIMARY KEY
+  (id)
+);
 
 
-CREATE TABLE categories (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  name varchar(100) NOT NULL,
+  CREATE TABLE categories
+  (
+    id int(11) NOT NULL
+    AUTO_INCREMENT,
+  name varchar
+    (100) NOT NULL,
   createdAt DATETIME,
   updatedAt DATETIME,
-  PRIMARY KEY (id)
-); 
+  PRIMARY KEY
+    (id)
+);
 
 
 
-CREATE TABLE colors (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  name varchar(500) NOT NULL,
+    CREATE TABLE colors
+    (
+      id int(11) NOT NULL
+      AUTO_INCREMENT,
+  name varchar
+      (500) NOT NULL,
   createdAt DATETIME,
   updatedAt DATETIME,
-  PRIMARY KEY (id)
-);  
+  PRIMARY KEY
+      (id)
+);
 
 
 
-CREATE TABLE tags (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  name varchar(500) NOT NULL,
+      CREATE TABLE tags
+      (
+        id int(11) NOT NULL
+        AUTO_INCREMENT,
+  name varchar
+        (500) NOT NULL,
   createdAt DATETIME,
   updatedAt DATETIME,
-  PRIMARY KEY (id)
-);  
+  PRIMARY KEY
+        (id)
+);
 
 
 
-CREATE TABLE users (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  firstName varchar(500) NOT NULL,
-  lastName varchar(500) NOT NULL,
-  email varchar(100) NOT NULL,
-  password varchar(100) NOT NULL,
-  avatar varchar(100) DEFAULT NULL,
+        CREATE TABLE users
+        (
+          id int(11) NOT NULL
+          AUTO_INCREMENT,
+  firstName varchar
+          (500) NOT NULL,
+  lastName varchar
+          (500) NOT NULL,
+  email varchar
+          (100) NOT NULL,
+  password varchar
+          (100) NOT NULL,
+  avatar varchar
+          (100) DEFAULT NULL,
   createdAt DATETIME,
   updatedAt DATETIME,
-  PRIMARY KEY (id)
-);  
+  PRIMARY KEY
+          (id)
+);
 
 
-CREATE TABLE addresses (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  street varchar(100) DEFAULT NULL,
-  number int(10) DEFAULT NULL,
-  city varchar(100) DEFAULT NULL,
-  province varchar(100) DEFAULT NULL,
-  zipCode varchar(100) DEFAULT NULL,
-  floor varchar(100) DEFAULT NULL,
-  apartment varchar(100) DEFAULT NULL,
-  phone varchar(100) DEFAULT NULL,
-  neighborhood varchar(100) DEFAULT NULL,
+          CREATE TABLE addresses
+          (
+            id int(11) NOT NULL
+            AUTO_INCREMENT,
+  street varchar
+            (100) DEFAULT NULL,
+  number int
+            (10) DEFAULT NULL,
+  city varchar
+            (100) DEFAULT NULL,
+  province varchar
+            (100) DEFAULT NULL,
+  zipCode varchar
+            (100) DEFAULT NULL,
+  floor varchar
+            (100) DEFAULT NULL,
+  apartment varchar
+            (100) DEFAULT NULL,
+  phone varchar
+            (100) DEFAULT NULL,
+  neighborhood varchar
+            (100) DEFAULT NULL,
   observations text DEFAULT NULL,
-  userId int(11) NOT NULL,
+  userId int
+            (11) NOT NULL,
   createdAt DATETIME,
   updatedAt DATETIME,
-  PRIMARY KEY (id),
-  foreign key (userId) references users (id)  
-); 
+  PRIMARY KEY
+            (id),
+  foreign key
+            (userId) references users
+            (id)  
+);
 
 
-CREATE TABLE products (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  name varchar(500) NOT NULL,
-  description varchar(500) DEFAULT NULL,
-  ingredients varchar(500) DEFAULT NULL,
-  price decimal(10,2) NOT NULL,
-  discount decimal(4,2) DEFAULT NULL,
-  weight decimal(10,2) DEFAULT NULL,
-  height decimal(10,2) DEFAULT NULL,
-  width decimal(10,2) DEFAULT NULL,
-  length decimal(10,2) DEFAULT NULL,
-  brandId int(11) NOT NULL,
-  categoryId int(11) NOT NULL,
+            CREATE TABLE products
+            (
+              id int(11) NOT NULL
+              AUTO_INCREMENT,
+  name varchar
+              (500) NOT NULL,
+  description varchar
+              (500) DEFAULT NULL,
+  ingredients varchar
+              (500) DEFAULT NULL,
+  price decimal
+              (10,2) NOT NULL,
+  discount decimal
+              (4,2) DEFAULT NULL,
+  weight decimal
+              (10,2) DEFAULT NULL,
+  height decimal
+              (10,2) DEFAULT NULL,
+  width decimal
+              (10,2) DEFAULT NULL,
+  length decimal
+              (10,2) DEFAULT NULL,
+  brandId int
+              (11) NOT NULL,
+  categoryId int
+              (11) NOT NULL,
   createdAt DATETIME,
   updatedAt DATETIME,
-  PRIMARY KEY (id),
-  FOREIGN KEY (categoryId) REFERENCES categories (id),
-  FOREIGN KEY (brandId) REFERENCES brands (id)
-);  
+  PRIMARY KEY
+              (id),
+  FOREIGN KEY
+              (categoryId) REFERENCES categories
+              (id),
+  FOREIGN KEY
+              (brandId) REFERENCES brands
+              (id)
+);
 
 
 
-CREATE TABLE purchases (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  userId int(11) NOT NULL,
+              CREATE TABLE purchases
+              (
+                id int(11) NOT NULL
+                AUTO_INCREMENT,
+  userId int
+                (11) NOT NULL,
   date date DEFAULT NULL,
-  total decimal(6,2) DEFAULT NULL,
-  payment varchar(100) DEFAULT NULL,
-  invoice varchar(100) DEFAULT NULL,
-  addressId int(11) NOT NULL,
+  total decimal
+                (6,2) DEFAULT NULL,
+  payment varchar
+                (100) DEFAULT NULL,
+  invoice varchar
+                (100) DEFAULT NULL,
+  addressId int
+                (11) NOT NULL,
   createdAt DATETIME,
   updatedAt DATETIME,
-  PRIMARY KEY (id),
-  FOREIGN KEY (addressId) REFERENCES addresses (id),
-  FOREIGN KEY (userId) REFERENCES users (id)
-); 
+  PRIMARY KEY
+                (id),
+  FOREIGN KEY
+                (addressId) REFERENCES addresses
+                (id),
+  FOREIGN KEY
+                (userId) REFERENCES users
+                (id)
+);
 
 
 
 
-CREATE TABLE user_product (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  userId int(11) NOT NULL,
-  productId int(11) NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (productId) REFERENCES products (id),
-  FOREIGN KEY (userId) REFERENCES users (id)
-);  
+                CREATE TABLE user_product
+                (
+                  id int(11) NOT NULL
+                  AUTO_INCREMENT,
+  userId int
+                  (11) NOT NULL,
+  productId int
+                  (11) NOT NULL,
+  PRIMARY KEY
+                  (id),
+  FOREIGN KEY
+                  (productId) REFERENCES products
+                  (id),
+  FOREIGN KEY
+                  (userId) REFERENCES users
+                  (id)
+);
 
 
 
-CREATE TABLE color_product (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  colorId int(11) NOT NULL,
-  productId int(11) NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (colorId) REFERENCES colors (id),
-  FOREIGN KEY (productId) REFERENCES products (id)
-);  
+                  CREATE TABLE color_product
+                  (
+                    id int(11) NOT NULL
+                    AUTO_INCREMENT,
+  colorId int
+                    (11) NOT NULL,
+  productId int
+                    (11) NOT NULL,
+  PRIMARY KEY
+                    (id),
+  FOREIGN KEY
+                    (colorId) REFERENCES colors
+                    (id),
+  FOREIGN KEY
+                    (productId) REFERENCES products
+                    (id)
+);
 
 
 
-CREATE TABLE images (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  route varchar(500) NOT NULL,
-  size varchar(500) DEFAULT NULL,
-  fileType varchar(500) DEFAULT NULL,
-  productId int(11) NOT NULL,
+                    CREATE TABLE images
+                    (
+                      id int(11) NOT NULL
+                      AUTO_INCREMENT,
+  route varchar
+                      (500) NOT NULL,
+  size varchar
+                      (500) DEFAULT NULL,
+  fileType varchar
+                      (500) DEFAULT NULL,
+  productId int
+                      (11) NOT NULL,
   createdAt DATETIME,
   updatedAt DATETIME,
-  PRIMARY KEY (id),
-  FOREIGN KEY (productId) REFERENCES products (id)
-);  
+  PRIMARY KEY
+                      (id),
+  FOREIGN KEY
+                      (productId) REFERENCES products
+                      (id)
+);
 
 
 
-CREATE TABLE product_purchase (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  price decimal(10,2) DEFAULT NULL,
-  quantity decimal(10,2) DEFAULT NULL,
-  productId int(11) NOT NULL,
-  purchaseId int(11) NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (productId) REFERENCES products (id),
-  FOREIGN KEY (purchaseId) REFERENCES purchases (id)
-);  
+                      CREATE TABLE product_purchase
+                      (
+                        id int(11) NOT NULL
+                        AUTO_INCREMENT,
+  price decimal
+                        (10,2) DEFAULT NULL,
+  quantity decimal
+                        (10,2) DEFAULT NULL,
+  productId int
+                        (11) NOT NULL,
+  purchaseId int
+                        (11) NOT NULL,
+  PRIMARY KEY
+                        (id),
+  FOREIGN KEY
+                        (productId) REFERENCES products
+                        (id),
+  FOREIGN KEY
+                        (purchaseId) REFERENCES purchases
+                        (id)
+);
 
 
-CREATE TABLE product_tag (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  tagId int(11) NOT NULL,
-  productId int(11) NOT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (tagId) REFERENCES tags (id),
-  FOREIGN KEY (productId) REFERENCES products (id)
+                        CREATE TABLE product_tag
+                        (
+                          id int(11) NOT NULL
+                          AUTO_INCREMENT,
+  tagId int
+                          (11) NOT NULL,
+  productId int
+                          (11) NOT NULL,
+  PRIMARY KEY
+                          (id),
+  FOREIGN KEY
+                          (tagId) REFERENCES tags
+                          (id),
+  FOREIGN KEY
+                          (productId) REFERENCES products
+                          (id)
 ); 
