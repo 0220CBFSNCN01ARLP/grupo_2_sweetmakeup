0c5eef6c8802ff4e8d5f7f04c2bcf18ae30b7904
agustinherlein <agustinherlein@hotmail.com>
2020-07-01 02:32:44 -0300

    arreglado error que evitaba que el header muestre el nombre del usuario

--- middlewares/rememberLoginMiddleware.js
+++ middlewares/rememberLoginMiddleware.js
@@ -9,7 +9,6 @@ const middleware = async function (req, res, next){
     if (req.cookies.user != undefined && req.session.user == undefined){
         let user = await User.findOne({
             where: {
-                email: req.cookies.user
             }
         });
         if (user == null) return res.redirect("register");
