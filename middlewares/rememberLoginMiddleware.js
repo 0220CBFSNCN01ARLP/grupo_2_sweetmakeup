22476271077bfbbe069d5a4b6182a2b1b57cc896
agustinherlein <agustinherlein@hotmail.com>
2020-07-01 02:32:04 -0300

    arreglado error que evitaba que el header muestre el nombre del usuario

--- middlewares/rememberLoginMiddleware.js
+++ middlewares/rememberLoginMiddleware.js
@@ -1,14 +1,16 @@
 const fs = require("fs");
 const path = require("path");
-const { getUsers, usersFilePath } = require("../utils/users");
+const {
+    User
+} = require("../database/models");
 
-const middleware = function (req, res, next){
-    const users = getUsers();
+const middleware = async function (req, res, next){
+    
     if (req.cookies.user != undefined && req.session.user == undefined){
-        const user = users.find((e) => {
-            return (
-               e.email == req.cookies.user
-            );
+        let user = await User.findOne({
+            where: {
+                email: req.cookies.user
+            }
         });
         if (user == null) return res.redirect("register");
         req.session.user = user;
