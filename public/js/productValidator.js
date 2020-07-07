2f231afbac630df453121651761f57dfe0e0be80
agustinherlein <agustinherlein@hotmail.com>
2020-07-07 15:33:58 -0300

    validacion de creacion de producto lista

--- public/js/productValidator.js
+++ public/js/productValidator.js
@@ -1,20 +1,59 @@
-alert("hola");
 
-function isNotEmpty(fieldId, errorMsgId) {
+let form = document.getElementById("createForm");
+
+function notEmpty(fieldId, errorMsgId, errors) {
   let field = document.getElementById(fieldId);
   let errorMsg = document.getElementById(errorMsgId);
-  if ((field.value == "")) {
-    console.log("alsdkfjalsdkfjañlsdkfj")
+  if (field.value == "") {
     errorMsg.style.display = "block";
+    field.style.borderColor = "red";
+    errors.push(`${fieldId} está vacío`)
+  } else {
+    errorMsg.style.display = "none";
   }
 }
 
-document.getElementById("productName").addEventListener("blur", ()=>{
-    isNotEmpty("productName", "nameEmptyMsg")
-})
-document.getElementById("price").addEventListener("blur", ()=>{
-    isNotEmpty("price", "priceEmptyMsg")
-})
-document.getElementById("description").addEventListener("blur", ()=>{
-    isNotEmpty("description", "descriptionEmptyMsg")
-})
+function selectNotEmpty(fieldId, errorMsgId, errors) {
+  let field = document.getElementById(fieldId);
+  let errorMsg = document.getElementById(errorMsgId);
+  if (field.value == "default") {
+    errorMsg.style.display = "block";
+    field.style.borderColor = "red";
+    errors.push(`${fieldId} está vacío`)
+  } else {
+    errorMsg.style.display = "none";
+  }
+}
+
+function minCharacters (fieldId, min, errorMsgId, errors) {
+  let field = document.getElementById(fieldId);
+  let errorMsg = document.getElementById(errorMsgId);
+  if (field.value != "" && field.value.length < min) {
+    errorMsg.style.display = "block";
+    field.style.borderColor = "red";
+    errors.push(`${fieldId} tiene menos de ${min} caracteres`)
+  } else {
+    errorMsg.style.display = "none";
+  }
+}
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  let errors = [];
+  console.log("hola")
+  notEmpty("productName", "nameEmptyMsg", errors);
+  notEmpty("price", "priceEmptyMsg", errors);
+  notEmpty("description", "descriptionEmptyMsg", errors);
+  selectNotEmpty("brand", "brandEmptyMsg", errors);
+  selectNotEmpty("thematic", "thematicEmptyMsg", errors);
+  minCharacters("productName", 3, "nameShortMsg", errors);
+  minCharacters("description", 10, "descriptionShortMsg", errors)
+  console.log(errors)
+
+  if (errors.length == 0){
+    form.submit();
+  } else {
+    document.documentElement.scrollTop = 0;
+  }
+  return false;
+});
