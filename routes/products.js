6853c54efeb7ef76fdc3d485ea5fbe6f5b18ae38
genaromalpeli <genamalpeli@gmail.com>
2020-07-11 13:45:25 -0300

    se genero conflicto, se resuelve

--- routes/products.js
+++ routes/products.js
@@ -8,19 +8,39 @@ const multer = require("multer");
 const { check, validationResult, body } = require("express-validator");
 
 var storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-        cb(null, "./public/img/products");
-    },
-    filename: function(req, file, cb) {
-        cb(
-            null,
-            file.fieldname + "-" + Date.now() + path.extname(file.originalname)
-        );
-    },
+  destination: function (req, file, cb) {
+    cb(null, "./public/img/productos");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
 });
 var upload = multer({
-    storage: storage,
+  storage: storage,
 });
+
+app.use(
+  expressValidator({
+    customValidators: {
+      isImage: function (value, filename) {
+        var extension = path.extname(filename).toLowerCase();
+        switch (extension) {
+          case ".jpg":
+            return ".jpg";
+          case ".jpeg":
+            return ".jpeg";
+          case ".png":
+            return ".png";
+          default:
+            return false;
+        }
+      },
+    },
+  })
+);
 
 // Creando desde cero un producto
 router.get("/create", authMiddleware, productController.create);
@@ -49,21 +69,15 @@ router.post(
 
 /*** EDIT ONE PRODUCT ***/
 router.get(
-    "/edit/:id",
-    authMiddleware,
-    productController.edit
+  "/edit/:id",
+  authMiddleware,
+  productController.edit
 ); /* GET - Form to create */
 router.put(
-<<<<<<< HEAD
-    "/edit/:id",
-    upload.any(),
-    productController.update
-=======
   "/edit/:id",
   upload.any(),
   [],
   productController.update
->>>>>>> 0d3c9e571b36c8041f95a37e15d6dd8b59a43e82
 ); /* PUT - Update in DB */
 
 // Borrando un producto
