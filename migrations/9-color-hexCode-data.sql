00299a1d5e6d94ded8ae7c8b814c9e8132512c86
genaromalpeli <genamalpeli@gmail.com>
2020-07-27 01:07:24 -0300

    subo poblacion de hexcode

---
+++ migrations/9-color-hexCode-data.sql
@@ -0,0 +1,884 @@
+use grupo_2_sweetmakeup;
+
+UPDATE colors 
+SET hexCode = '#f6db91'
+where id = 1;
+
+UPDATE colors 
+SET hexCode = '#1c0cda'
+where id = 2;
+
+UPDATE colors 
+SET hexCode = '#503296'
+where id = 3;
+
+UPDATE colors 
+SET hexCode = '#16e249'
+where id = 4;
+
+UPDATE colors 
+SET hexCode = '#80e1a0'
+where id = 5;
+
+UPDATE colors 
+SET hexCode = '#e9213c'
+where id = 6;
+
+UPDATE colors 
+SET hexCode = '#6d9b49'
+where id = 7;
+
+UPDATE colors 
+SET hexCode = '#b6dc1e'
+where id = 8;
+
+UPDATE colors 
+SET hexCode = '#067ea3'
+where id = 9;
+
+UPDATE colors 
+SET hexCode = '#f9501c'
+where id = 10;
+
+UPDATE colors 
+SET hexCode = '#fc81a8'
+where id = 11;
+
+UPDATE colors 
+SET hexCode = '#b04475'
+where id = 12;
+
+UPDATE colors 
+SET hexCode = '#05dd23'
+where id = 13;
+
+UPDATE colors 
+SET hexCode = '#3686a4'
+where id = 14;
+
+UPDATE colors 
+SET hexCode = '#4bd552'
+where id = 15;
+
+UPDATE colors 
+SET hexCode = '#4312d2'
+where id = 16;
+
+UPDATE colors 
+SET hexCode = '#856bae'
+where id = 17;
+
+UPDATE colors 
+SET hexCode = '#85e074'
+where id = 18;
+
+UPDATE colors 
+SET hexCode = '#d580d2'
+where id = 19;
+
+UPDATE colors 
+SET hexCode = '#12d990'
+where id = 20;
+
+UPDATE colors 
+SET hexCode = '#c5595b'
+where id = 21;
+
+UPDATE colors 
+SET hexCode = '#7e2c0d'
+where id = 22;
+
+UPDATE colors 
+SET hexCode = '#62b5c7'
+where id = 23;
+
+UPDATE colors 
+SET hexCode = '#242f08'
+where id = 24;
+
+UPDATE colors 
+SET hexCode = '#85a2eb'
+where id = 25;
+
+UPDATE colors 
+SET hexCode = '#0609b9'
+where id = 26;
+
+UPDATE colors 
+SET hexCode = '#140a92'
+where id = 27;
+
+UPDATE colors 
+SET hexCode = '#d32fd0'
+where id = 28;
+
+UPDATE colors 
+SET hexCode = '#3cc131'
+where id = 29;
+
+UPDATE colors 
+SET hexCode = '#de5cbf'
+where id = 30;
+
+UPDATE colors 
+SET hexCode = '#6401e7'
+where id = 31;
+
+UPDATE colors 
+SET hexCode = '#c0d08b'
+where id = 32;
+
+UPDATE colors 
+SET hexCode = '#c14fb6'
+where id = 33;
+
+UPDATE colors 
+SET hexCode = '#b36c41'
+where id = 34;
+
+UPDATE colors 
+SET hexCode = '#04302e'
+where id = 35;
+
+UPDATE colors 
+SET hexCode = '#132a21'
+where id = 36;
+
+UPDATE colors 
+SET hexCode = '#9fc5ff'
+where id = 37;
+
+UPDATE colors 
+SET hexCode = '#140447'
+where id = 38;
+
+UPDATE colors 
+SET hexCode = '#3fe3ad'
+where id = 39;
+
+UPDATE colors 
+SET hexCode = '#f2c022'
+where id = 40;
+
+UPDATE colors 
+SET hexCode = '#374753'
+where id = 41;
+
+UPDATE colors 
+SET hexCode = '#ed0d28'
+where id = 42;
+
+UPDATE colors 
+SET hexCode = '#de41a2'
+where id = 43;
+
+UPDATE colors 
+SET hexCode = '#2155d3'
+where id = 44;
+
+UPDATE colors 
+SET hexCode = '#72dbd5'
+where id = 45;
+
+UPDATE colors 
+SET hexCode = '#2203c4'
+where id = 46;
+
+UPDATE colors 
+SET hexCode = '#81a3c1'
+where id = 47;
+
+UPDATE colors 
+SET hexCode = '#7f7020'
+where id = 48;
+
+UPDATE colors 
+SET hexCode = '#c964db'
+where id = 49;
+
+UPDATE colors 
+SET hexCode = '#8fef19'
+where id = 50;
+
+UPDATE colors 
+SET hexCode = '#601cc6'
+where id = 51;
+
+UPDATE colors 
+SET hexCode = '#0e524c'
+where id = 52;
+
+UPDATE colors 
+SET hexCode = '#89c925'
+where id = 53;
+
+UPDATE colors 
+SET hexCode = '#bc53fa'
+where id = 54;
+
+UPDATE colors 
+SET hexCode = '#38df34'
+where id = 55;
+
+UPDATE colors 
+SET hexCode = '#8522d5'
+where id = 56;
+
+UPDATE colors 
+SET hexCode = '#6fe63d'
+where id = 57;
+
+UPDATE colors 
+SET hexCode = '#b70643'
+where id = 58;
+
+UPDATE colors 
+SET hexCode = '#f5d57e'
+where id = 59;
+
+UPDATE colors 
+SET hexCode = '#38312b'
+where id = 60;
+
+UPDATE colors 
+SET hexCode = '#dd7cef'
+where id = 61;
+
+UPDATE colors 
+SET hexCode = '#da2527'
+where id = 62;
+
+UPDATE colors 
+SET hexCode = '#17736f'
+where id = 63;
+
+UPDATE colors 
+SET hexCode = '#dc8ce1'
+where id = 64;
+
+UPDATE colors 
+SET hexCode = '#fb0f92'
+where id = 65;
+
+UPDATE colors 
+SET hexCode = '#a18d49'
+where id = 66;
+
+UPDATE colors 
+SET hexCode = '#c1382a'
+where id = 67;
+
+UPDATE colors 
+SET hexCode = '#829c61'
+where id = 68;
+
+UPDATE colors 
+SET hexCode = '#3b991e'
+where id = 69;
+
+UPDATE colors 
+SET hexCode = '#016169'
+where id = 70;
+
+UPDATE colors 
+SET hexCode = '#c92789'
+where id = 71;
+
+UPDATE colors 
+SET hexCode = '#2e7d92'
+where id = 72;
+
+UPDATE colors 
+SET hexCode = '#2ab7bc'
+where id = 73;
+
+UPDATE colors 
+SET hexCode = '#3bc25a'
+where id = 74;
+
+UPDATE colors 
+SET hexCode = '#4a8929'
+where id = 75;
+
+UPDATE colors 
+SET hexCode = '#618424'
+where id = 76;
+
+UPDATE colors 
+SET hexCode = '#f0a25a'
+where id = 77;
+
+UPDATE colors 
+SET hexCode = '#b20b37'
+where id = 78;
+
+UPDATE colors 
+SET hexCode = '#5fbe35'
+where id = 79;
+
+UPDATE colors 
+SET hexCode = '#2054f8'
+where id = 80;
+
+UPDATE colors 
+SET hexCode = '#7e3133'
+where id = 81;
+
+UPDATE colors 
+SET hexCode = '#50979e'
+where id = 82;
+
+UPDATE colors 
+SET hexCode = '#5b803b'
+where id = 83;
+
+UPDATE colors 
+SET hexCode = '#94f36a'
+where id = 84;
+
+UPDATE colors 
+SET hexCode = '#5e27f2'
+where id = 85;
+
+UPDATE colors 
+SET hexCode = '#f64190'
+where id = 86;
+
+UPDATE colors 
+SET hexCode = '#0befbf'
+where id = 87;
+
+UPDATE colors 
+SET hexCode = '#84cc2e'
+where id = 88;
+
+UPDATE colors 
+SET hexCode = '#8d013e'
+where id = 89;
+
+UPDATE colors 
+SET hexCode = '#943cbb'
+where id = 90;
+
+UPDATE colors 
+SET hexCode = '#8e2bf7'
+where id = 91;
+
+UPDATE colors 
+SET hexCode = '#6d3672'
+where id = 92;
+
+UPDATE colors 
+SET hexCode = '#7e6aa5'
+where id = 93;
+
+
+UPDATE colors 
+SET hexCode = '#fb732c'
+where id = 94;
+
+
+UPDATE colors 
+SET hexCode = '#1ef968'
+where id = 95;
+
+
+UPDATE colors 
+SET hexCode = '#2a2c66'
+where id = 96;
+
+
+UPDATE colors 
+SET hexCode = '#4acbc1'
+where id = 97;
+
+
+UPDATE colors 
+SET hexCode = '#3ec93f'
+where id = 98;
+
+
+UPDATE colors 
+SET hexCode = '#b0c226'
+where id = 99;
+
+
+UPDATE colors 
+SET hexCode = '#bc0e24'
+where id = 100;
+
+
+UPDATE colors 
+SET hexCode = '#bf23e9'
+where id = 101;
+
+
+UPDATE colors 
+SET hexCode = '#f3897d'
+where id = 102;
+
+
+UPDATE colors 
+SET hexCode = '#1cdc2b'
+where id = 103;
+
+
+UPDATE colors 
+SET hexCode = '#e93c12'
+where id = 104;
+
+
+UPDATE colors 
+SET hexCode = '#ab8482'
+where id = 105;
+
+
+UPDATE colors 
+SET hexCode = '#826eee'
+where id = 106;
+
+
+UPDATE colors 
+SET hexCode = '#693992'
+where id = 107;
+
+
+UPDATE colors 
+SET hexCode = '#24b367'
+where id = 108;
+
+
+UPDATE colors 
+SET hexCode = '#bd3ef6'
+where id = 109;
+
+
+UPDATE colors 
+SET hexCode = '#653744'
+where id = 110;
+
+
+UPDATE colors 
+SET hexCode = '#89e508'
+where id = 111;
+
+
+UPDATE colors 
+SET hexCode = '#37b3bd'
+where id = 112;
+
+
+UPDATE colors 
+SET hexCode = '#0481a5'
+where id = 113;
+
+
+UPDATE colors 
+SET hexCode = '#4f981b'
+where id = 114;
+
+
+UPDATE colors 
+SET hexCode = '#b1afd0'
+where id = 115;
+
+
+UPDATE colors 
+SET hexCode = '#0e5dec'
+where id = 116;
+
+
+UPDATE colors 
+SET hexCode = '#3e77ee'
+where id = 117;
+
+
+UPDATE colors 
+SET hexCode = '#9059cc'
+where id = 118;
+
+
+UPDATE colors 
+SET hexCode = '#51da9d'
+where id = 119;
+
+
+UPDATE colors 
+SET hexCode = '#0ebca5'
+where id = 120;
+
+
+UPDATE colors 
+SET hexCode = '#9ab65d'
+where id = 121;
+
+
+UPDATE colors 
+SET hexCode = '#f32e4f'
+where id = 122;
+
+
+UPDATE colors 
+SET hexCode = '#dab7c2'
+where id = 123;
+
+
+UPDATE colors 
+SET hexCode = '#23d290'
+where id = 124;
+
+
+UPDATE colors 
+SET hexCode = '#c5a9b3'
+where id = 125;
+
+
+UPDATE colors 
+SET hexCode = '#9a650a'
+where id = 126;
+
+
+UPDATE colors 
+SET hexCode = '#59374d'
+where id = 127;
+
+
+UPDATE colors 
+SET hexCode = '#09b2b8'
+where id = 128;
+
+
+UPDATE colors 
+SET hexCode = '#1b8fc2'
+where id = 129;
+
+
+UPDATE colors 
+SET hexCode = '#1a0ca2'
+where id = 130;
+
+
+UPDATE colors 
+SET hexCode = '#cdc4c9'
+where id = 131;
+
+
+UPDATE colors 
+SET hexCode = '#f1e255'
+where id = 132;
+
+
+UPDATE colors 
+SET hexCode = '#0080b6'
+where id = 133;
+
+
+UPDATE colors 
+SET hexCode = '#651b1c'
+where id = 134;
+
+
+UPDATE colors 
+SET hexCode = '#96a5e0'
+where id = 135;
+
+
+UPDATE colors 
+SET hexCode = '#cd0fcf'
+where id = 136;
+
+
+UPDATE colors 
+SET hexCode = '#f59d75'
+where id = 137;
+
+
+UPDATE colors 
+SET hexCode = '#6e4b48'
+where id = 138;
+
+
+UPDATE colors 
+SET hexCode = '#229526'
+where id = 139;
+
+
+UPDATE colors 
+SET hexCode = '#57d2b3'
+where id = 140;
+
+
+UPDATE colors 
+SET hexCode = '#16db97'
+where id = 141;
+
+
+UPDATE colors 
+SET hexCode = '#cfd74c'
+where id = 142;
+
+
+UPDATE colors 
+SET hexCode = '#380341'
+where id = 143;
+
+
+UPDATE colors 
+SET hexCode = '#c03bd1'
+where id = 144;
+
+
+UPDATE colors 
+SET hexCode = '#4f26cc'
+where id = 145;
+
+
+UPDATE colors 
+SET hexCode = '#f18350'
+where id = 146;
+
+
+UPDATE colors 
+SET hexCode = '#8692f0'
+where id = 147;
+
+
+UPDATE colors 
+SET hexCode = '#1e6b92'
+where id = 148;
+
+
+UPDATE colors 
+SET hexCode = '#56d246'
+where id = 149;
+
+
+UPDATE colors 
+SET hexCode = '#9ee101'
+where id = 150;
+
+
+UPDATE colors 
+SET hexCode = '#255838'
+where id = 151;
+
+
+UPDATE colors 
+SET hexCode = '#a4e1a5'
+where id = 152;
+
+
+UPDATE colors 
+SET hexCode = '#9bc71c'
+where id = 153;
+
+
+UPDATE colors 
+SET hexCode = '#d3e2b3'
+where id = 154;
+
+
+UPDATE colors 
+SET hexCode = '#0feda7'
+where id = 155;
+
+
+UPDATE colors 
+SET hexCode = '#d71c9b'
+where id = 156;
+
+
+UPDATE colors 
+SET hexCode = '#9e20c2'
+where id = 157;
+
+
+UPDATE colors 
+SET hexCode = '#3d06bd'
+where id = 158;
+
+
+UPDATE colors 
+SET hexCode = '#eebac0'
+where id = 159;
+
+
+UPDATE colors 
+SET hexCode = '#3995a8'
+where id = 160;
+
+
+UPDATE colors 
+SET hexCode = '#f20051'
+where id = 161;
+
+
+UPDATE colors 
+SET hexCode = '#b0e0a9'
+where id = 162;
+
+
+UPDATE colors 
+SET hexCode = '#4379ce'
+where id = 163;
+
+
+UPDATE colors 
+SET hexCode = '#e40de3'
+where id = 164;
+
+
+UPDATE colors 
+SET hexCode = '#6edb02'
+where id = 165;
+
+
+UPDATE colors 
+SET hexCode = '#3de0d6'
+where id = 166;
+
+
+UPDATE colors 
+SET hexCode = '#d22d64'
+where id = 167;
+
+
+UPDATE colors 
+SET hexCode = '#f87c5f'
+where id = 168;
+
+
+UPDATE colors 
+SET hexCode = '#3c8c18'
+where id = 169;
+
+
+UPDATE colors 
+SET hexCode = '#c27616'
+where id = 170;
+
+
+UPDATE colors 
+SET hexCode = '#becf94'
+where id = 171;
+
+
+UPDATE colors 
+SET hexCode = '#5f1e06'
+where id = 172;
+
+
+UPDATE colors 
+SET hexCode = '#528c9b'
+where id = 173;
+
+
+UPDATE colors 
+SET hexCode = '#9f2e28'
+where id = 174;
+
+
+UPDATE colors 
+SET hexCode = '#a2e172'
+where id = 175;
+
+UPDATE colors 
+SET hexCode = '#11bdc7'
+where id = 176;
+
+UPDATE colors 
+SET hexCode = '#76e283'
+where id = 177;
+
+UPDATE colors 
+SET hexCode = '#047ab6'
+where id = 178;
+
+UPDATE colors 
+SET hexCode = '#b9326c'
+where id = 179;
+
+UPDATE colors 
+SET hexCode = '#e9c2fc'
+where id = 180;
+
+UPDATE colors 
+SET hexCode = '#351f15'
+where id = 181;
+
+UPDATE colors 
+SET hexCode = '#dfb09e'
+where id = 182;
+
+UPDATE colors 
+SET hexCode = '#c2857a'
+where id = 183;
+
+UPDATE colors 
+SET hexCode = '#566bb6'
+where id = 184;
+
+UPDATE colors 
+SET hexCode = '#b450e7'
+where id = 185;
+
+UPDATE colors 
+SET hexCode = '#117a3d'
+where id = 186;
+
+UPDATE colors 
+SET hexCode = '#516086'
+where id = 187;
+
+UPDATE colors 
+SET hexCode = '#b21f17'
+where id = 188;
+
+UPDATE colors 
+SET hexCode = '#541790'
+where id = 189;
+
+UPDATE colors 
+SET hexCode = '#b7e68c'
+where id = 190;
+
+UPDATE colors 
+SET hexCode = '#b3c39d'
+where id = 191;
+
+UPDATE colors 
+SET hexCode = '#13a4cd'
+where id = 192;
+
+UPDATE colors 
+SET hexCode = '#f885dc'
+where id = 193;
+
+UPDATE colors 
+SET hexCode = '#db4710'
+where id = 194;
+
+UPDATE colors 
+SET hexCode = '#4dfb37'
+where id = 195;
+
+UPDATE colors 
+SET hexCode = '#b7adca'
+where id = 196;
+
+UPDATE colors 
+SET hexCode = '#a70524'
+where id = 197;
+
+UPDATE colors 
+SET hexCode = '#0b8b7d'
+where id = 198;
+
+UPDATE colors 
+SET hexCode = '#bc807f'
+where id = 199;
+
+UPDATE colors 
+SET hexCode = '#bc807f'
+where id = 200;
+
