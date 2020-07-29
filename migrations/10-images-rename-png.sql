626c0234f99797475c8dcb83852ce678482280c5
genaromalpeli <genamalpeli@gmail.com>
2020-07-29 01:28:46 -0300

    nuevas fotos png. modifico la estetica del index y de las sectios. elimino fb y tw para inicio de sesion

---
+++ migrations/10-images-rename-png.sql
@@ -0,0 +1,807 @@
+use grupo_2_sweetmakeup;
+
+update
+	images
+set
+	fileType = "png";
+
+	
+update
+	images
+set
+	productId = 1,
+	route = "product_1.png"
+where
+	id = 1;
+
+update
+	images
+set
+	productId = 2,
+	route = "product_2.png"
+where
+	id = 2;
+
+update
+	images
+set
+	productId = 3,
+	route = "product_3.png"
+where
+	id = 3;
+
+update
+	images
+set
+	productId = 4,
+	route = "product_4.png"
+where
+	id = 4;
+
+update
+	images
+set
+	productId = 5,
+	route = "product_5.png"
+where
+	id = 5;
+
+update
+	images
+set
+	productId = 6,
+	route = "product_6.png"
+where
+	id = 6;
+
+update
+	images
+set
+	productId = 7,
+	route = "product_7.png"
+where
+	id = 7;
+
+update
+	images
+set
+	productId = 8,
+	route = "product_8.png"
+where
+	id = 8;
+
+update
+	images
+set
+	productId = 9,
+	route = "product_9.png"
+where
+	id = 9;
+
+update
+	images
+set
+	productId = 10,
+	route = "product_10.png"
+where
+	id = 10;
+
+update
+	images
+set
+	productId = 11,
+	route = "product_11.png"
+where
+	id = 11;
+
+update
+	images
+set
+	productId = 12,
+	route = "product_12.png"
+where
+	id = 12;
+
+update
+	images
+set
+	productId = 13,
+	route = "product_13.png"
+where
+	id = 13;
+
+update
+	images
+set
+	productId = 14,
+	route = "product_14.png"
+where
+	id = 14;
+
+update
+	images
+set
+	productId = 15,
+	route = "product_15.png"
+where
+	id = 15;
+
+update
+	images
+set
+	productId = 16,
+	route = "product_16.png"
+where
+	id = 16;
+
+update
+	images
+set
+	productId = 17,
+	route = "product_17.png"
+where
+	id = 17;
+
+update
+	images
+set
+	productId = 18,
+	route = "product_18.png"
+where
+	id = 18;
+
+update
+	images
+set
+	productId = 19,
+	route = "product_19.png"
+where
+	id = 19;
+
+update
+	images
+set
+	productId = 20,
+	route = "product_20.png"
+where
+	id = 20;
+
+update
+	images
+set
+	productId = 21,
+	route = "product_21.png"
+where
+	id = 21;
+
+update
+	images
+set
+	productId = 22,
+	route = "product_22.png"
+where
+	id = 22;
+
+update
+	images
+set
+	productId = 23,
+	route = "product_23.png"
+where
+	id = 23;
+
+update
+	images
+set
+	productId = 24,
+	route = "product_24.png"
+where
+	id = 24;
+
+update
+	images
+set
+	productId = 25,
+	route = "product_25.png"
+where
+	id = 25;
+
+update
+	images
+set
+	productId = 26,
+	route = "product_26.png"
+where
+	id = 26;
+
+update
+	images
+set
+	productId = 27,
+	route = "product_27.png"
+where
+	id = 27;
+
+update
+	images
+set
+	productId = 28,
+	route = "product_28.png"
+where
+	id = 28;
+
+update
+	images
+set
+	productId = 29,
+	route = "product_29.png"
+where
+	id = 29;
+
+update
+	images
+set
+	productId = 30,
+	route = "product_30.png"
+where
+	id = 30;
+
+update
+	images
+set
+	productId = 31,
+	route = "product_31.png"
+where
+	id = 31;
+
+update
+	images
+set
+	productId = 32,
+	route = "product_32.png"
+where
+	id = 32;
+
+update
+	images
+set
+	productId = 33,
+	route = "product_33.png"
+where
+	id = 33;
+
+update
+	images
+set
+	productId = 34,
+	route = "product_34.png"
+where
+	id = 34;
+
+update
+	images
+set
+	productId = 35,
+	route = "product_35.png"
+where
+	id = 35;
+
+update
+	images
+set
+	productId = 36,
+	route = "product_36.png"
+where
+	id = 36;
+
+update
+	images
+set
+	productId = 37,
+	route = "product_37.png"
+where
+	id = 37;
+
+update
+	images
+set
+	productId = 38,
+	route = "product_38.png"
+where
+	id = 38;
+
+update
+	images
+set
+	productId = 39,
+	route = "product_39.png"
+where
+	id = 39;
+
+update
+	images
+set
+	productId = 40,
+	route = "product_40.png"
+where
+	id = 40;
+
+update
+	images
+set
+	productId = 41,
+	route = "product_41.png"
+where
+	id = 41;
+
+update
+	images
+set
+	productId = 42,
+	route = "product_42.png"
+where
+	id = 42;
+
+update
+	images
+set
+	productId = 43,
+	route = "product_43.png"
+where
+	id = 43;
+
+update
+	images
+set
+	productId = 44,
+	route = "product_44.png"
+where
+	id = 44;
+
+update
+	images
+set
+	productId = 45,
+	route = "product_45.png"
+where
+	id = 45;
+
+update
+	images
+set
+	productId = 46,
+	route = "product_46.png"
+where
+	id = 46;
+
+update
+	images
+set
+	productId = 47,
+	route = "product_47.png"
+where
+	id = 47;
+
+update
+	images
+set
+	productId = 48,
+	route = "product_48.png"
+where
+	id = 48;
+
+update
+	images
+set
+	productId = 49,
+	route = "product_49.png"
+where
+	id = 49;
+
+update
+	images
+set
+	productId = 50,
+	route = "product_50.png"
+where
+	id = 50;
+
+update
+	images
+set
+	productId = 51,
+	route = "product_51.png"
+where
+	id = 51;
+
+update
+	images
+set
+	productId = 52,
+	route = "product_52.png"
+where
+	id = 52;
+
+update
+	images
+set
+	productId = 53,
+	route = "product_53.png"
+where
+	id = 53;
+
+update
+	images
+set
+	productId = 54,
+	route = "product_54.png"
+where
+	id = 54;
+
+update
+	images
+set
+	productId = 55,
+	route = "product_55.png"
+where
+	id = 55;
+
+update
+	images
+set
+	productId = 56,
+	route = "product_56.png"
+where
+	id = 56;
+
+update
+	images
+set
+	productId = 57,
+	route = "product_57.png"
+where
+	id = 57;
+
+update
+	images
+set
+	productId = 58,
+	route = "product_58.png"
+where
+	id = 58;
+
+update
+	images
+set
+	productId = 59,
+	route = "product_59.png"
+where
+	id = 59;
+
+update
+	images
+set
+	productId = 60,
+	route = "product_60.png"
+where
+	id = 60;
+
+update
+	images
+set
+	productId = 61,
+	route = "product_61.png"
+where
+	id = 61;
+
+update
+	images
+set
+	productId = 62,
+	route = "product_62.png"
+where
+	id = 62;
+
+update
+	images
+set
+	productId = 63,
+	route = "product_63.png"
+where
+	id = 63;
+
+update
+	images
+set
+	productId = 64,
+	route = "product_64.png"
+where
+	id = 64;
+
+update
+	images
+set
+	productId = 65,
+	route = "product_65.png"
+where
+	id = 65;
+
+update
+	images
+set
+	productId = 66,
+	route = "product_66.png"
+where
+	id = 66;
+
+update
+	images
+set
+	productId = 67,
+	route = "product_67.png"
+where
+	id = 67;
+
+update
+	images
+set
+	productId = 68,
+	route = "product_68.png"
+where
+	id = 68;
+
+update
+	images
+set
+	productId = 69,
+	route = "product_69.png"
+where
+	id = 69;
+
+update
+	images
+set
+	productId = 70,
+	route = "product_70.png"
+where
+	id = 70;
+
+update
+	images
+set
+	productId = 71,
+	route = "product_71.png"
+where
+	id = 71;
+
+update
+	images
+set
+	productId = 72,
+	route = "product_72.png"
+where
+	id = 72;
+
+update
+	images
+set
+	productId = 73,
+	route = "product_73.png"
+where
+	id = 73;
+
+update
+	images
+set
+	productId = 74,
+	route = "product_74.png"
+where
+	id = 74;
+
+update
+	images
+set
+	productId = 75,
+	route = "product_75.png"
+where
+	id = 75;
+
+update
+	images
+set
+	productId = 76,
+	route = "product_76.png"
+where
+	id = 76;
+
+update
+	images
+set
+	productId = 77,
+	route = "product_77.png"
+where
+	id = 77;
+
+update
+	images
+set
+	productId = 78,
+	route = "product_78.png"
+where
+	id = 78;
+
+update
+	images
+set
+	productId = 79,
+	route = "product_79.png"
+where
+	id = 79;
+
+update
+	images
+set
+	productId = 80,
+	route = "product_80.png"
+where
+	id = 80;
+
+update
+	images
+set
+	productId = 81,
+	route = "product_81.png"
+where
+	id = 81;
+
+update
+	images
+set
+	productId = 82,
+	route = "product_82.png"
+where
+	id = 82;
+
+update
+	images
+set
+	productId = 83,
+	route = "product_83.png"
+where
+	id = 83;
+
+update
+	images
+set
+	productId = 84,
+	route = "product_84.png"
+where
+	id = 84;
+
+update
+	images
+set
+	productId = 85,
+	route = "product_85.png"
+where
+	id = 85;
+
+update
+	images
+set
+	productId = 86,
+	route = "product_86.png"
+where
+	id = 86;
+
+update
+	images
+set
+	productId = 87,
+	route = "product_87.png"
+where
+	id = 87;
+
+update
+	images
+set
+	productId = 88,
+	route = "product_88.png"
+where
+	id = 88;
+
+update
+	images
+set
+	productId = 89,
+	route = "product_89.png"
+where
+	id = 89;
+
+update
+	images
+set
+	productId = 90,
+	route = "product_90.png"
+where
+	id = 90;
+
+update
+	images
+set
+	productId = 91,
+	route = "product_91.png"
+where
+	id = 91;
+
+update
+	images
+set
+	productId = 92,
+	route = "product_92.png"
+where
+	id = 92;
+
+update
+	images
+set
+	productId = 93,
+	route = "product_93.png"
+where
+	id = 93;
+
+update
+	images
+set
+	productId = 94,
+	route = "product_94.png"
+where
+	id = 94;
+
+update
+	images
+set
+	productId = 95,
+	route = "product_95.png"
+where
+	id = 95;
+
+update
+	images
+set
+	productId = 96,
+	route = "product_96.png"
+where
+	id = 96;
+
+update
+	images
+set
+	productId = 97,
+	route = "product_97.png"
+where
+	id = 97;
+
+update
+	images
+set
+	productId = 98,
+	route = "product_98.png"
+where
+	id = 98;
+
+update
+	images
+set
+	productId = 99,
+	route = "product_99.png"
+where
+	id = 99;
+
+update
+	images
+set
+	productId = 100,
+	route = "product_100.png"
+where
+	id = 100;
