e8c4a72474ca0798eb84edbdcbf1f4afd643d234
agustinherlein <agustinherlein@hotmail.com>
2020-08-01 14:46:21 -0300

    la imagen desaparece de la vista al ser eliminada

---
+++ public/js/imageDelete.js
@@ -0,0 +1,13 @@
+let deleteButtons = document.getElementsByClassName("imgDeleteBtn");
+
+for (let i = 0; i < deleteButtons.length; i++) {
+  let cardId = "imgCard" + i;
+  let btn = deleteButtons[i];
+  let card = document.getElementById(cardId);
+  console.log(btn);
+  console.log(card);
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    card.remove();
+  });
+}
